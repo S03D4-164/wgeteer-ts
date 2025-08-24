b08976e4cd6187937fa8607778dc5387d2b2c370
S03D4-164 <12199393+S03D4-164@users.noreply.github.com>
2025-08-24 16:30:39 +0900

    feat🚀: Add Playwright action and screenshot utilities

---
+++ app/utils/playwgetAction.ts
@@ -0,0 +1,86 @@
+import { yaraSource } from './yara';
+import logger from './logger';
+import {
+  saveFullscreenshot,
+  imgResize,
+  cdpScreenshot,
+} from './playwgetScreenshot';
+import mongoose from 'mongoose';
+
+async function playwgetAction(page: any, webpage: any, client: any) {
+  const pageId = webpage._id;
+  const delay = Number(webpage.option.delay) * 1000;
+  // execute actions
+  let actions;
+  let yararule = await yaraSource(await page.content());
+  if (yararule?.actions) {
+    logger.debug(`[${pageId}] ${yararule}`);
+    actions = yararule.actions;
+    webpage.yara = yararule;
+  }
+  if (webpage.option.actions) {
+    actions = webpage.option.actions;
+  }
+  if (actions && actions.length > 1) {
+    webpage.option.actions = actions;
+    const lines = actions.split('\r\n');
+    let limit = 5;
+    let ssarray: any[] = [];
+    for (let line of lines) {
+      // screenshot before action
+      let ssobj: any = {};
+      let screenshot = await cdpScreenshot(client);
+      const resizedImg = await imgResize(screenshot);
+      if (resizedImg) {
+        ssobj.thumbnail = resizedImg.toString('base64');
+      }
+      //let fullscreenshot = screenshot;
+      let fss = await saveFullscreenshot(screenshot);
+      if (fss) {
+        ssobj.full = new mongoose.Types.ObjectId(fss);
+      }
+      //console.log(ssobj);
+      ssarray.push(ssobj);
+      // actions
+      let elem = line.split('>');
+      let action = elem[0]?.trim();
+      let target = elem[1]?.trim();
+      let input = elem[2]?.trim();
+      let last = elem[3]?.trim();
+      logger.debug(`[${pageId}] action: ${action}, target: ${target}`);
+      let options = {
+        timeout: delay,
+      };
+      if (action == 'eval') {
+        await page.evaluate(target, options);
+      } else {
+        let loc = page.locator(target);
+        if (action == 'clicktxt') {
+          loc = page.getByText(target);
+        }
+        if (last == 'last') loc = loc.last();
+        else loc = loc.first();
+        if (action == 'click' || action == 'clicktxt') {
+          await loc.click(options);
+        } else if (action == 'fill') {
+          await loc.fill(input, options);
+        } else if (action == 'press') {
+          await loc.press(input, options);
+        }
+      }
+      //await new Promise((done) => setTimeout(done, delay));
+      limit--;
+      if (limit <= 0) break;
+    }
+    //console.log(ssarray);
+    if (ssarray.length > 0) {
+      webpage.screenshots = ssarray;
+    }
+    await new Promise((done) => setTimeout(done, delay));
+    return;
+  } else {
+    return;
+  }
+}
+
+export { playwgetAction };
---
+++ app/utils/playwgetScreenshot.ts
@@ -0,0 +1,50 @@
+import ScreenshotModel from '../models/screenshot';
+import Jimp from 'jimp';
+import crypto from 'crypto';
+import logger from './logger';
+
+async function imgResize(buffer: Buffer): Promise<Buffer> {
+  let image = await Jimp.read(buffer);
+  if (image.getWidth() > 240) {
+    //res.resize(240, Jimp.AUTO);
+    let resized = image.resize(240, Jimp.AUTO);
+    image = resized.crop(0, 0, 240, 135);
+  }
+  return image.getBufferAsync(Jimp.MIME_PNG);
+}
+
+async function saveFullscreenshot(buff: Buffer): Promise<string | undefined> {
+  try {
+    const md5Hash = crypto.createHash('md5').update(buff).digest('hex');
+    const fullscreenshot = buff.toString('base64');
+
+    let ss: any = await ScreenshotModel.findOneAndUpdate(
+      { md5: md5Hash },
+      { screenshot: fullscreenshot },
+      { new: true, upsert: true, strict: true },
+    ).exec();
+
+    if (ss) {
+      return ss._id.toString();
+    } else {
+      logger.warn('Screenshot not saved.');
+      return undefined;
+    }
+  } catch (err: any) {
+    logger.error(err);
+    return undefined;
+  }
+}
+
+async function cdpScreenshot(client: any) {
+  const base64ss = (
+    await client.send('Page.captureScreenshot', {
+      captureBeyondViewport: true,
+      optimizeForSpeed: true,
+    })
+  ).data;
+  let screenshot = Buffer.from(base64ss, 'base64');
+  return screenshot;
+}
+
+export { saveFullscreenshot, imgResize, cdpScreenshot };
