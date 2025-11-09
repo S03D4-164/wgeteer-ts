1fc96755b9f3727ce429f6ec0936a8de70007218
S03D4-164 <12199393+S03D4-164@users.noreply.github.com>
2025-11-09 13:58:36 +0900

    refactor♻️: Extract pptrEventSet function to its own module

--- app/utils/playwget.ts
+++ app/utils/playwget.ts
@@ -9,7 +9,6 @@ import RequestModel from '../models/request';
 import ResponseModel from '../models/response';
 
 import logger from './logger';
-import { savePayload } from './playwgetSave';
 import { saveRequest, saveResponse } from './playwgetIntercept';
 import {
   cdpScreenshot,
@@ -28,108 +27,9 @@ import { Xvfb } from './node-xvfb';
 import { spawn } from 'node:child_process';
 import cleanup from './playwgetCleanup';
 import { playwgetAction } from './playwgetAction';
+import pptrEventSet from './playwgetEvent';
 
 const dataDir = '/tmp/ppengo';
-
-async function pptrEventSet(
-  browserContext: BrowserContext,
-  page: Page,
-  webpage: any,
-): Promise<void> {
-  const pageId = webpage._id;
-  /*
-  page.on('request', (request: any) => {
-    //logger.debug(`Request: ${request.url()}`);
-  });
-  page.on('requestfinished', async (request: any) => {
-    const res = await request.response();
-    logger.debug(`Finished: ${res.url()}`);
-  });
-  page.on('requestfailed', (request: any) => {
-    logger.debug(`Failed: ${request.failure().errorText} ${request.url()}`);
-  });
-    */
-  const browser = browserContext.browser();
-  if (browser) {
-    browser.once('disconnected', () =>
-      logger.debug(`[${pageId}] browser disconnected`),
-    );
-  }
-  browserContext.once('close', () =>
-    logger.debug(`[${pageId}] browserContext closed`),
-  );
-  page.once('crash', async (page: any) => {
-    logger.error(`[${pageId}] Page crashed: ${page.url()}`);
-  });
-  page.once('domcontentloaded', async (page: any) => {
-    logger.debug(`[${pageId}] DOM content loaded: ${page.url()}`);
-  });
-  page.once('close', async (page: any) => {
-    logger.debug(`[${pageId}] Page closed: ${page.url()}`);
-  });
-  page.once('load', async (page: any) => {
-    logger.debug(`[${pageId}] Page loaded: ${page.url()}`);
-  });
-  page.on('dialog', (dialog) => dialog.dismiss());
-  // Log all uncaught errors to the terminal
-  page.on('pageerror', (exception) => {
-    logger.error(`[${pageId}] Uncaught exception: "${exception}"`);
-  });
-  // download pdf
-  if (webpage.option.pdf) {
-    await page.route('**/*', async (route, request) => {
-      if (
-        request.resourceType() === 'document' &&
-        route.request().url().endsWith('.pdf')
-      ) {
-        const response = await page.context().request.get(request.url());
-        await route.fulfill({
-          response,
-          headers: {
-            ...response.headers(),
-            'Content-Disposition': 'attachment',
-          },
-        });
-      } else {
-        route.continue();
-      }
-    });
-  }
-  page.once('download', async (download) => {
-    logger.info(`Download started: ${download.url()}`);
-    async function readableToBuffer(readable: any): Promise<Buffer> {
-      return new Promise((resolve, reject) => {
-        const chunks: any = [];
-        readable.on('data', (chunk: Buffer) => {
-          logger.debug(`Received ${chunk.length} bytes of data.`);
-          chunks.push(chunk);
-        });
-        readable.on('error', () => {
-          logger.error('Error reading download stream');
-          reject;
-        });
-        readable.on('end', () => {
-          logger.info('Download stream ended.');
-          resolve(Buffer.concat(chunks));
-        });
-      });
-    }
-    const read = await download.createReadStream();
-    try {
-      const buffer: any = await readableToBuffer(read);
-      logger.debug(`Downloaded ${buffer.length} bytes of data.`);
-      const payloadId: any = await savePayload(buffer);
-      if (payloadId) {
-        logger.debug(`Payload saved with ID: ${payloadId}`);
-        webpage.payload = payloadId;
-        webpage.error = 'A file has been downloaded.';
-        await webpage.save();
-      }
-    } catch (err) {
-      logger.error(`Error processing download: ${err}`);
-    }
-  });
-}
 
 async function genPage(
   webpage: any,
